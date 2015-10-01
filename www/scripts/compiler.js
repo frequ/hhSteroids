4bdfe1e7e578e389d3a6d61aa2beb5d56dcd24c7
frequ <samppa.valkama@gmail.com>
2015-10-01 13:15:12 +0300

    new files: compiler for events and helper functions

---
+++ www/scripts/compiler.js
@@ -0,0 +1,24 @@
+angular.module('SteroidsApplication')
+    .directive('compile', function($compile) {
+
+        return {
+            restrict: 'A',
+            replace: true,
+            link: function(scope, element, attrs)  {
+
+                scope.$watch(attrs.compile, function(html) {
+
+                    //fugly hack but does the trick
+                    //if there is no externalUrl in event obj, then it seems to come here as a string
+                    if (attrs.url && attrs.url !== "event.externalUrl") {
+                        var index = html.indexOf('<button') +7;
+                        html = html.slice(0,index) + " ng-click=\"openUrl("+attrs.url+")\" " + html.slice(index);
+                    }
+
+                    element.html(html);
+                    $compile(element.contents())(scope);
+                }, true);
+
+            }
+        };
+    });
---
+++ www/scripts/helpers.js
@@ -0,0 +1,26 @@
+angular.module('SteroidsApplication')
+    .factory('helpers', function() {
+
+        return {
+            makeComparable: function(str) {
+                return str.replace(/ä/g, "a").replace(/ö/g, "o").split(' ').join('-').toLowerCase();
+            },
+
+            constructSplitting: function(splittable, divisible) {
+                var returnArr = [];
+                var start = 0;
+                var end = divisible;
+
+                for(var i = 0; i < Math.ceil(splittable.length/divisible); i++) {
+
+                    returnArr.push(splittable.slice(start, end));
+
+                    start += divisible;
+                    end += divisible;
+
+                }
+                return returnArr;
+            }
+
+        };
+    });
